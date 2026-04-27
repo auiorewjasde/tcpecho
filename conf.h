b747a8b9b295930d38f8e245279156e56f7f93b6
sane <46612011+auiorewjasde@users.noreply.github.com>
2026-04-28 00:24:09 +0900

    Update conf.h

--- conf.h
+++ conf.h
@@ -1,7 +1,7 @@
 #ifndef CONF_F
 #define CONF_F
 #define PORT 12345
-#define BUFSIZE 16
-#define SCANFTXT "%16[^\n]" 
+#define BUFSIZE 2000
+#define SCANFTXT "%2000[^\n]"
 #define SERVERIP "127.0.0.1"
 #endif
